75f090c15473b21c1a1d17790094a27d530013d7
juc096 <juc096@ucsd.edu>
2020-12-10 12:17:16 -0800

    fragment practice

---
+++ app/src/main/java/com/example/fragment_pract/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fragment_pract;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
